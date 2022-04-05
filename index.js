d4edbec576478b6b067b218a619437a9c4195558
jesse zenon <jesse.zenon@gmail.com>
2022-04-04 23:02:41 -0500

    Done

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1 = 1;
+let num2 = 62;
+
+const multiply = (num1 * num2)
+
+const random = Math.floor(Math.random()* 10)+1;
+
+let num3 = 10
+let num4 = 6
+
+const mod = (num3 % num4);
+
+
+
+let max = Math.max(1, 2, 3, 20);
